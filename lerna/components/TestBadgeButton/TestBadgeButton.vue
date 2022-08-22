8aed2831988d1036e8f427c2dd13a664654da3ea
Jiří Procházka <ji.prochazka@gmail.com>
2022-08-22 13:28:08 +0200

    Test

---
+++ lerna/components/TestBadgeButton/TestBadgeButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps({
+  badge: {
+    type: String,
+  },
+  label: {
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <q-badge color="orange" text-color="black" :label="badge" />
+    <q-btn color="primary" :label="label" />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
